b2069f5790cc12511b381db11873e0e0d67ba368
xunan <xunan@pingchang666.com>
2018-06-06 14:59:34 +0800

    初始化项目

---
+++ XNTodayNews/XNTodayNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XNTodayNews
+//
+//  Created by Dandre on 2018/6/6.
+//  Copyright © 2018年 Dandre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
